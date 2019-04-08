256afe821eb28e5472aca66af823adb3a8aa3750
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2019-04-08 23:27:49 +0200

    fix path for windows dll src

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -13,7 +13,7 @@ class RtMidiTestConan(ConanFile):
         cmake.build()
 
     def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dll", dst="bin", src="lib")
         self.copy("*.dylib*", dst="bin", src="lib")
         self.copy('*.so*', dst='bin', src='lib')
 
